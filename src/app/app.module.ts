46b9dea0fae4a9d88b0de73b17639219e1d530df
Svetlana Bahlay <conbka@gmail.com>
2021-06-15 17:03:29 +0300

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchRecipeComponent } from './search-recipe/search-recipe.component';
+import { RecipeImageComponent } from './recipe-image/recipe-image.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchRecipeComponent,
+    RecipeImageComponent,
+    RecipesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: SearchRecipeComponent },
+      { path: 'recipes', component: RecipesComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
